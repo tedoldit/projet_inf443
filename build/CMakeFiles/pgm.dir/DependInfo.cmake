bc39af005f2bc7dfdcb1fa5614ebae04cd2533a5
Teo Tedoldi <teo.tedoldi@polytechnique.edu>
2020-05-13 08:45:42 +0200

    Base du projet

---
+++ build/CMakeFiles/pgm.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/teo/Documents/INF443/projet/main/helper_scene/helper_scene.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/main/helper_scene/helper_scene.cpp.o"
+  "/home/teo/Documents/INF443/projet/main/main.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/main/main.cpp.o"
+  "/home/teo/Documents/INF443/projet/main/scene_base/base.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/main/scene_base/base.cpp.o"
+  "/home/teo/Documents/INF443/projet/scenes/modeling.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/scenes/modeling.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/glad/src/glad.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/glad/src/glad.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/imgui/imgui.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/imgui/imgui.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/imgui/imgui_demo.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/imgui/imgui_demo.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/imgui/imgui_draw.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/imgui/imgui_draw.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/imgui/imgui_impl_glfw.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/imgui/imgui_impl_glfw.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/imgui/imgui_impl_opengl3.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/imgui/imgui_impl_opengl3.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/imgui/imgui_widgets.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/imgui/imgui_widgets.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/lodepng/lodepng.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/lodepng/lodepng.cpp.o"
+  "/home/teo/Documents/INF443/projet/third_party/simplexnoise/simplexnoise1234.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/third_party/simplexnoise/simplexnoise1234.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/base/basic_types/basic_types.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/base/basic_types/basic_types.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/base/error/error.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/base/error/error.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/base/file/file.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/base/file/file.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/base/rand/rand.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/base/rand/rand.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/base/string/string.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/base/string/string.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/camera/camera.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/camera/camera.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/camera_control_glfw/camera_control_glfw.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/camera_control_glfw/camera_control_glfw.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/picking/info/picking_info.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/picking/info/picking_info.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/picking/primitives/picking_primitives.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/picking/primitives/picking_primitives.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/picking/ray/picking_ray.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/picking/ray/picking_ray.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/screen_motion/screen_motion.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/screen_motion/screen_motion.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/time_period/time_period.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/time_period/time_period.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/interaction/time_slider/time_slider.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/interaction/time_slider/time_slider.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/helper_functions/linspace/linspace.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/helper_functions/linspace/linspace.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/helper_functions/norm/norm.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/helper_functions/norm/norm.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/mat/mat2/mat2.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/mat/mat2/mat2.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/mat/mat3/mat3.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/mat/mat3/mat3.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/mat/mat4/mat4.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/mat/mat4/mat4.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/transformation/affine_transform/affine_transform.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/transformation/affine_transform/affine_transform.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/transformation/special_transform/special_transform.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/transformation/special_transform/special_transform.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/vec/vec2/vec2.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/vec/vec2/vec2.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/vec/vec3/vec3.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/vec/vec3/vec3.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/math/vec/vec4/vec4.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/math/vec/vec4/vec4.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/opengl/debug/opengl_debug.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/opengl/debug/opengl_debug.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/opengl/shader/shader.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/opengl/shader/shader.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/opengl/texture/image/image.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/opengl/texture/image/image.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/opengl/texture/texture_gpu/texture_gpu.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/opengl/texture/texture_gpu/texture_gpu.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/opengl/uniform/uniform.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/opengl/uniform/uniform.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/curve/curve_drawable/curve_drawable.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/curve/curve_drawable/curve_drawable.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/curve/curve_dynamic_drawable/curve_dynamic_drawable.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/curve/curve_dynamic_drawable/curve_dynamic_drawable.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/curve/curve_gpu/curve_gpu.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/curve/curve_gpu/curve_gpu.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/curve/curve_primitive/curve_primitive.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/curve/curve_primitive/curve_primitive.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/hierarchy_mesh/hierarchy_mesh_drawable/hierarchy_mesh_drawable.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/hierarchy_mesh/hierarchy_mesh_drawable/hierarchy_mesh_drawable.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/hierarchy_mesh/hierarchy_mesh_drawable/hierarchy_mesh_drawable_display_skeleton/hierarchy_mesh_drawable_display_skeleton.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/hierarchy_mesh/hierarchy_mesh_drawable/hierarchy_mesh_drawable_display_skeleton/hierarchy_mesh_drawable_display_skeleton.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/hierarchy_mesh/hierarchy_mesh_drawable/hierarchy_mesh_drawable_node/hierarchy_mesh_drawable_node.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/hierarchy_mesh/hierarchy_mesh_drawable/hierarchy_mesh_drawable_node/hierarchy_mesh_drawable_node.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/mesh/mesh_drawable/mesh_drawable.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/mesh/mesh_drawable/mesh_drawable.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/mesh/mesh_drawable/mesh_drawable_gpu_data/mesh_drawable_gpu_data.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/mesh/mesh_drawable/mesh_drawable_gpu_data/mesh_drawable_gpu_data.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/mesh/mesh_drawable/mesh_drawable_uniform/mesh_drawable_uniform.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/mesh/mesh_drawable/mesh_drawable_uniform/mesh_drawable_uniform.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/mesh/mesh_loader/obj/obj.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/mesh/mesh_loader/obj/obj.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/mesh/mesh_primitive/mesh_primitive.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/mesh/mesh_primitive/mesh_primitive.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/mesh/mesh_structure/mesh.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/mesh/mesh_structure/mesh.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/segment/segment_drawable_immediate_mode/segment_drawable_immediate_mode.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/segment/segment_drawable_immediate_mode/segment_drawable_immediate_mode.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/segment/segments_drawable/segments_drawable.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/segment/segments_drawable/segments_drawable.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/segment/segments_drawable/segments_drawable_uniform/segments_drawable_uniform.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/segment/segments_drawable/segments_drawable_uniform/segments_drawable_uniform.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/shape/segment/segments_gpu/segments_gpu.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/shape/segment/segments_gpu/segments_gpu.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/wrapper/glfw/events/events.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/wrapper/glfw/events/events.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/wrapper/glfw/fps_counter/fps_counter.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/wrapper/glfw/fps_counter/fps_counter.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/wrapper/glfw/window/window.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/wrapper/glfw/window/window.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/wrapper/imgui/imgui.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/wrapper/imgui/imgui.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/wrapper/lodepng/lodepng.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/wrapper/lodepng/lodepng.cpp.o"
+  "/home/teo/Documents/INF443/projet/vcl/wrapper/perlin/perlin.cpp" "/home/teo/Documents/INF443/projet/build/CMakeFiles/pgm.dir/vcl/wrapper/perlin/perlin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../third_party/eigen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ scenes/current_scene.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+// Indicate here which exercise should be currently considered using its define keyword
+#define SCENE_3D_GRAPHICS // replace this keyword to change the scene
